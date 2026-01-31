9d309bd617cc431f5441ab0479a8c57f24f75abb
pratik0987 <52742153+pratik0987@users.noreply.github.com>
2026-01-31 13:48:36 +0000

    operations

--- basics/3_conversionoperation.js
+++ basics/3_conversionoperation.js
@@ -21,5 +21,25 @@ let booleanIsLoggedIn = Boolean(isLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// **********operations***********
+
+let value = 3
+let negValue = -value
+// console.log(negValue)
+
+let str1 = "hello "
+
+let str2 = "world"
+ let str3 = str1 + str2
+//  console.log(str3)
+
+let gameCounter =100
+gameCounter++;
+console.log(gameCounter)
+
+
+
+ 
