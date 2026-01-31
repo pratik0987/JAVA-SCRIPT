513b353abd6c42e0edec9a15529fabcc8d5f5766
pratik0987 <52742153+pratik0987@users.noreply.github.com>
2026-01-31 12:46:33 +0000

    data or var

---
+++ basics/1_variable.js
@@ -0,0 +1,12 @@
+const accountId = 144553
+let accountEmail = "prateek@gmail.com"
+var accountPassword = "12345"
+accountCity = "Raipur"
+let accountState;
+// accountId = 2 
+accountEmail = "jshdhjdjf.com"
+accountPassword = "84746"
+accountCity = "dmt"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
---
+++ basics/2_datatypes.js
@@ -0,0 +1,26 @@
+"use strict"; //treat all js code as newer version
+
+// alert (3+3) //we are using nodejs,not browser
+
+console.log(3+3) 
+console.log("prateek");
+
+let age = 27
+let isLoggedIn = false
+let state ;
+
+//number => 2 to power 53
+//bigint
+//string =>""
+//boolean=> true or false
+//null=> standlone value
+//undefined =>
+//symbol
+
+
+//object
+
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+
